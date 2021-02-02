07386b82491c98760fcbcdfbc12fab65da9ab057
shivank <shivankkunwar100@gmail.com>
2021-02-02 15:08:59 +0530

    final with file upload error

--- APP/exercises/js/index.js
+++ APP/exercises/js/index.js
@@ -8,8 +8,40 @@ window.onload = () => {
 }
 
 const getPosts = () => {
+    fetch(API_URL,{
+        method: 'GET'
+    }).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        buildPosts(data);
+    })
 }
 
 const buildPosts = (blogPosts) => {
-
+    blogPostsContent ="";
+    for(blogPost of blogPosts){
+        const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
+        postImage =`${API_BASE_URL}${blogPost.post_image}`
+        const postLink =`./post.html?id=${blogPost.id}`
+     blogPostsContent +=` 
+                <a class="postlink" href= "${postLink}">   
+                    <div class="post">
+                    <div class="post-image" style ="background-image: url(${postImage})">
+                        
+                    </div>
+                    <div class="post-content">
+                        <div class="post-date">
+                            ${postDate}
+                        </div>
+                        <div class="post-title"><h4>${blogPost.title}</h4>
+                            
+                        </div>
+                        <div class="post-text">
+                                 ${blogPost.content}                   
+                        </div>
+                    </div>
+                </div>
+                </a>`
+    }
+    document.querySelector(".blogpost").innerHTML = blogPostsContent;
 }
--- APP/exercises/js/post.js
+++ APP/exercises/js/post.js
@@ -10,12 +10,38 @@ const API_BASE_URL = "http://localhost:3000/";
 window.onload = () => {
     getPost();
 }
+const getPostIdParam = () =>{
+    const queryString =window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get("id")
+    
+}
 
 const getPost = () => {
+    const postId =getPostIdParam();
     // CODE GOES HERE
+    const url = `${API_URL}${postId}`;
+    fetch(url,{
+        method:'GET'
+    }).then((response) =>{
+        return response.json();
+    }).then((data)=>{
+
+        buildPost(data);
+    })
+
+
 }
 
 const buildPost = (data) => {
-    // HINT: Convert the date number to a Date string 
+//     // HINT: Convert the date number to a Date string 
+
+        const postDate = new Date(parseInt(data.added_date)).toDateString();
+        postImage =`${API_BASE_URL}${data.post_image}`
+        document.querySelector("header").style.backgroundImage=`url(${postImage})`
+        document.getElementById("individual-post-title").innerText = data.title;
+        document.getElementById("individual-post-date").innerText =  `Published on ${postDate}`;
+        document.getElementById("individual-post-content").innerText = data.content; 
+//  
 }
 
